3524d29e15602834ac052238f7cbec4850b4a8d6
jhigger <njohnjhigger@gmail.com>
2024-03-14 19:08:27 +0800

    fix vercel build error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH ?? ""}`;
+}
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
