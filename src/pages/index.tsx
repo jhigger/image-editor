d2f89aa32cca0649d3025d05cb0d2c12a6f6d8c9
jhigger <njohnjhigger@gmail.com>
2024-03-14 19:20:36 +0800

    add head meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import ImageEditor from "~/components/ImageEditor";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <ImageEditor />
-    </div>
+    <>
+      <Head>
+        <title>Image Editor</title>
+        <meta name="description" content="Image Editor made by Kairos" />
+        <link rel="icon" href="/kairos.png" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <ImageEditor />
+      </main>
+    </>
   );
 }
